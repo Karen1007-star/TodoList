bdd4cb027174290cba3ca6bbc2c9737a27fc8846
karen1007-star <karen_08_121@hotmail.com>
2026-02-13 15:11:40 -0500

    html8 y JS8

---
+++ app8.js
@@ -0,0 +1,8 @@
+console.log("conectado")
+const formulario8=document.getElementById("formulario8")
+const textActivi = document.getElementById("textoActividades")
+const agregar=document.getElementById("agregar")
+const ulLista=document.getElementById("ListaFormulario8")
+const eliminar = document.getElementById("eliminar")
+const editar = document.getElementById("editar")
+
