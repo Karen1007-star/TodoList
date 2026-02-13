796a02d258f733a96dd70d2108f0f4e1d432d393
Karen <karen_08_121@hotmail.com>
2026-02-13 10:46:10 -0500

    subi el html6

---
+++ app6.js
@@ -0,0 +1,17 @@
+const formulario1= document.querySelector(".formulario1")
+const texto1 = document.getElementById("texto1")
+const agregar = document.getElementById("agregar")
+const listaPadre=document.querySelector(".listaPadre")
+const eliminar = document.querySelector(".eliminar")
+const editar = document.querySelector(".editar")
+
+formulario1.addEventListener("submit",(e)=>{
+    e.preventDefault();
+    const valor = texto1.value;
+    const listaNueva = document.createElement("li")
+    listaNueva.textContent = valor;
+    listaPadre.appendChild(listaNueva)    
+})
+
+
+
